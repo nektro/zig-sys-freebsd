df09dcbeb808532e544b65d636344e1ca919171f
Meghan Denny <hello@nektro.net>
2026-08-19 23:22:46 -0700

    init

---
+++ build.zig
@@ -0,0 +1,27 @@
+const std = @import("std");
+const deps = @import("./deps.zig");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const mode = b.option(std.builtin.OptimizeMode, "mode", "") orelse .Debug;
+    const disable_llvm = b.option(bool, "disable_llvm", "use the non-llvm zig codegen") orelse false;
+
+    const tests = b.addTest(.{
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("test.zig"),
+            .target = target,
+            .optimize = mode,
+        }),
+    });
+    deps.addAllTo(tests);
+    tests.root_module.link_libc = true;
+    tests.use_llvm = !disable_llvm;
+    tests.use_lld = !disable_llvm;
+    b.getInstallStep().dependOn(&tests.step);
+
+    const test_step = b.step("test", "Run all library tests");
+    const tests_run = b.addRunArtifact(tests);
+    tests_run.setCwd(b.path("."));
+    tests_run.has_side_effects = true;
+    test_step.dependOn(&tests_run.step);
+}
---
+++ test.zig
@@ -0,0 +1,2 @@
+const std = @import("std");
+const sys_freebsd = @import("sys-freebsd");
